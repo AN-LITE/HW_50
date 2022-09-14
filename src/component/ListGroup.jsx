ec4367a27b1b227edca712a22f26c9ceccc6a5bd
Andrey Gurenko <89606947+AN-LITE@users.noreply.github.com>
2022-09-14 14:18:27 +0300

    second

--- src/component/ListGroup.jsx
+++ src/component/ListGroup.jsx
@@ -1,30 +1,8 @@
-//
-//
-// import React from "react";
-//
-// function ListGroup (props) {
-//     const { children } = props
-//     return (
-//         <ul className="list-group">
-//             {React.Children.map(children, (child) => {
-//                 return <li className="list-group-item">{child}</li>
-//             })}
-//         </ul>
-//     )
-// }
-//
-// export default ListGroup
-
-// <ul className="list-group">
-//     {this.props.children.map(item => <li className="list-group-item" key={id++}>{item}</li>)}
-// </ul>
-
 import React from "react";
 
 class ListGroup extends React.Component{
 
         render() {
-
             let id = 0;
             return(
 
@@ -33,7 +11,7 @@ class ListGroup extends React.Component{
                         <li className="list-group-item list-group-item-action list-group-item-secondary"
                             key={id++}>{item}
                         </li>
-                    )}в
+                    )}
 
                 </ul>
             )
